ae50f9e531a237ab8a04d2220774136eed130acc
sosssolasticot <solene.gilloppe@orange.fr>
2022-11-28 11:10:03 -0500

    page services ajout articles

--- templates/template-services.php
+++ templates/template-services.php
@@ -8,44 +8,60 @@ Template Name: Services
 
 
 <section>
-<h2>Services</h2>
-<div> 
-    <?php
-    $args = array(
+    <h2>Services</h2>
+    <div> 
+        <?php
+        $args = array(
         'post_type' => 'post',
-        'category_name' => 'Service',
-    );
-    $query = new WP_Query($args);
-    ?>
-    <?php if ($query->have_posts()): ?>
-        <?php while ($query->have_posts()): $query->the_post(); ?>
-            <div class="ecole-info"> <?php the_content(); ?> </div>
-        <?php endwhile; ?>
-        <?php endif; ?>
+        'tag_name' => 'services',
+        );
+        $query = new WP_Query($args);
+        ?>
+        <?php if ($query->have_posts()): ?>
+            <?php while ($query->have_posts()): $query->the_post(); ?>
+                <div class="ecole-info"> <?php the_content(); ?> </div>
+            <?php endwhile; ?>
+            <?php endif; ?>
     </div>
+    <div>
     <div class="widget services">
-    <?php
-    $args = array(
-        'post_type' => 'Service',
-    );
-    $query = new WP_Query($args);
-    ?>
-    <?php if ($query->have_posts()): ?>
-    <?php while ($query->have_posts()):
+        <?php
+        $args = array(
+            'post_type' => 'Service',
+        );
+        $query = new WP_Query($args);
+        ?>
+        <?php if ($query->have_posts()): ?>
+        <?php while ($query->have_posts()):
             $query->the_post(); ?>
-    <article class="item-third">
-        <img src="<?php the_field('image_du_services_'); ?>" alt="">
-        <h3>
-            <?php the_field('titre_service_'); ?>
-        </h3>
-        <p>
-            <?php the_field('description_services_') ?>
-        </p>
-    </article>
-    <?php endwhile; ?>
-    <?php endif;
-    wp_reset_postdata();
-    ?>
+        <article class="item-third">
+            <img src="<?php the_field('image_du_services_'); ?>" alt="">
+            <h3>
+                <?php the_field('titre_service_'); ?>
+            </h3>
+            <p>
+                <?php the_field('description_services_') ?>
+            </p>
+        </article>
+        <?php endwhile; ?>
+        <?php endif;
+        wp_reset_postdata();
+        ?>
+    </div>
+        <div>
+        <?php
+        $args = array(
+        'post_type' => 'post',
+        'tag_name' => 'imageservices',
+        );
+        $query = new WP_Query($args);
+        ?>
+        <?php if ($query->have_posts()): ?>
+            <?php while ($query->have_posts()): $query->the_post(); ?>
+                <div class="ecole-info"> <?php the_content(); ?> </div>
+            <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
     </div>
 </section>
 

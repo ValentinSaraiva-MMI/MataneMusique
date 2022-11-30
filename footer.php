ee322009e25c6dda100932c4f5362e328b4d431a
Hatehorde <maxime.paquette03@gmail.com>
2022-11-30 09:26:05 -0500

    changement footer

--- footer.php
+++ footer.php
@@ -1,15 +1,6 @@
 </main>
 <footer>
         <div class="content">
-            <!-- <div class="top">
-                <div class="logo-details">
-                    <span class="logo_name">Ecole Musique Matane</span>
-                </div>
-                <div class="media-icons">
-                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#"><i class="fab fa-youtube"></i></a>
-                </div>
-            </div> -->
             <div class="link-boxes">
                 <ul class="box">
                     <li class="link_name">Nous joindre</li><br>
@@ -39,12 +30,6 @@
                     <li><input type="text" placeholder="Votre adresse couriel"></li>
                     <li><input type="button" value="s'inscrire"></li>
                 </ul>
-            </div>
-        </div>
-        <div class="bottom-details">
-            <div class="bottom_text">
-                <span class="copyright_text">Copyright © 2022 <a href="#">Ecole Musique Matane.</a>Tout droit 
-                    résevé</span>
             </div>
         </div>
 </footer>

1b467ebbabf719438bd79a8ed97f9e549eca886e
sosssolasticot <solene.gilloppe@orange.fr>
2022-12-08 14:52:09 -0500

    actu

--- templates/template-actualite.php
+++ templates/template-actualite.php
@@ -1,14 +1,13 @@
 <?php /* Template Name: Gabarit Actualitée */?>
 
-    <?php get_header(); ?>
-
+<?php get_header(); ?>
     
 <?php
-$args = array(
-    'post_type' => 'Actualités',
-    'posts_per_page' => 3
-);
-$query = new WP_query($args);
+    $args = array(
+        'post_type' => 'Actualités',
+        'posts_per_page' => 3
+    );
+    $query = new WP_query($args);
 ?>
 
 <div class="position-actu-evnmt nouvelles">
@@ -42,67 +41,64 @@ $query = new WP_query($args);
         </div>
         <!-- Test animation btn bordure -->
         <div class="test-btn">
-                    <a href="http://marcel-pirnay.be/">
-                    <svg>
-                        <defs>
-                            <linearGradient id="grad1">
-                                <stop offset="0%" stop-color="#dc0019"/>
-                                <stop offset="100%" stop-color="#ff8e01" />
-                            </linearGradient>
-                        </defs>
-                        <rect x="5" y="5" rx="20" fill="none" stroke="url(#grad1)"></rect>
-                    </svg>
-                    <!--<span>Voir mes réalisations</span>-->
-                        <span>Découvrir plus de nouvelles</span>
-                    </a>
-                </div>
+            <a href="http://marcel-pirnay.be/">
+                <svg>
+                    <defs>
+                        <linearGradient id="grad1">
+                            <stop offset="0%" stop-color="#dc0019"/>
+                            <stop offset="100%" stop-color="#ff8e01" />
+                        </linearGradient>
+                    </defs>
+                    <rect x="5" y="5" rx="20" fill="none" stroke="url(#grad1)"></rect>
+                </svg>
+                <span> Découvrir plus d'actualités </span>
+            </a>
+        </div>
     </section>
 
     <?php
-    $args = array(
-        'post_type' => 'Evenement',
-        'posts_per_page' => 3,
-    );
-    $query = new WP_query($args);
+        $args = array(
+            'post_type' => 'Evenement',
+            'posts_per_page' => 3,
+        );
+        $query = new WP_query($args);
     ?>
-
 
     <section>
         <h2>Évenements à venir</h2>
         <div class="position-evenement">
             <?php if ($query->have_posts()): ?>
-            <?php while ($query->have_posts()):
-                    $query->the_post(); ?>
-            <article>
-                <div class="evenement-position">
-                    <img src="<?php the_field('image_evenement') ?>" alt="">
-                    <div class="evenement">
-                        <div class="evenement-infos">    
-                            <div class="evenement-categorie">
-                                <?php the_field('categorie_evenement_'); ?>
-                            </div>
-                            <div>
-                                <?php the_field('paragraphe_evenement_'); ?>
-                            </div>
-                        </div>
-                        <div class="date-lieu">
-                            <div class="evenement-date">
-                                <p> DATE </p>
-                                <div>
-                                    <?php the_field('date_evenement_'); ?>
+                <?php while ($query->have_posts()): $query->the_post(); ?>
+                    <article>
+                        <div class="evenement-position">
+                            <img src="<?php the_field('image_evenement') ?>" alt="">
+                            <div class="evenement">
+                                <div class="evenement-infos">    
+                                    <div class="evenement-categorie">
+                                        <?php the_field('categorie_evenement_'); ?>
+                                    </div>
+                                    <div>
+                                        <?php the_field('paragraphe_evenement_'); ?>
+                                    </div>
+                                </div>
+                                <div class="date-lieu">
+                                    <div class="evenement-date">
+                                        <p> DATE </p>
+                                        <div>
+                                            <?php the_field('date_evenement_'); ?>
+                                        </div>
+                                    </div>
+                                    <div class="evenement-lieu">
+                                        <p> LIEU </p>
+                                        <div>
+                                            <?php the_field('lieu_evenement_'); ?>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="evenement-lieu">
-                                <p> LIEU </p>
-                                <div>
-                                    <?php the_field('lieu_evenement_'); ?>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> 
-            </article>
-            <?php endwhile; ?>
+                        </div> 
+                    </article>
+                <?php endwhile; ?>
             <?php endif; ?>
         </div>
     </section>

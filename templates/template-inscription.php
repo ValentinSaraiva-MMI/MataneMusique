1f689fe1e83c5c8001516548c28103816e058ebe
Hatehorde <maxime.paquette03@gmail.com>
2022-12-08 11:11:44 -0500

    page inscription

--- templates/template-inscription.php
+++ templates/template-inscription.php
@@ -7,6 +7,36 @@ Template Name: Formulaire Inscription
 
 <?php get_header(); ?>
 
+<?php if (have_posts()) : ?>
 
+
+
+<?php while (have_posts()) : the_post(); ?>
+
+
+
+    <article class="inscription-container">
+
+        <div class="inscription-width">
+
+            <h2><?php the_title(); ?></h2>
+
+            <p><?php the_content(); ?></p>
+
+        </div>
+
+    </article>
+
+
+
+<?php endwhile; ?>
+
+
+
+<?php else : ?>
+
+
+
+<?php endif; ?>
 
 <?php get_footer(); ?>

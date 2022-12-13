cf30a6c2a43e11a0ee0d1b4890e41a665fcf2380
Hatehorde <maxime.paquette03@gmail.com>
2022-12-13 09:49:44 -0500

    template membre

--- templates/template-membre.php
+++ templates/template-membre.php
@@ -10,9 +10,26 @@ Template Name: Page Membre
 
 <?php echo get_the_password_form(); ?>
 
-    
-
 <?php else : ?>
+    <?php
+        $args = array(
+        'post_type' => 'post',
+        'tag' => 'membre',
+        );
+        $query = new WP_Query($args);
+    ?>
+    <?php if ($query->have_posts()): ?>
+        <?php while ($query->have_posts()): $query->the_post(); ?>
+            <div>
+                <h3> 
+                    <?php the_title(); ?> 
+                </h3>
+                <div> 
+                    <?php the_content(); ?> 
+                </div>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
 <?php endif ?>
 <?php 
 wp_reset_postdata(); 

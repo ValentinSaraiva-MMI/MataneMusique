3743d26775637416540cab189b256762026c4030
valentinsa <valentinsa0701@gmail.com>
2022-12-13 10:16:51 -0500

    css page 404

--- 404.php
+++ 404.php
@@ -1,7 +1,12 @@
 <?php get_header(); ?>
+<div class="page-404">
+
+
+
 <h1>404</h1>
-<p>La page que vous avez demandé est introuvable.</p>
+<p class="p-404">La page que vous avez demandé est introuvable.</p>
 <a href="<?php echo esc_url(home_url('/')); ?>">
-               <button>Retour accueil </button>
+               <button class="btn-404">RETOUR À L'ACCUEIL </button>
                 </a>
+                </div>
 <?php get_footer(); ?>

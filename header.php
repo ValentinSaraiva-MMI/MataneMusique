1d34d2436952e8a2aec7f2e1e7bef217beee924a
Hatehorde <maxime.paquette03@gmail.com>
2022-12-13 10:26:39 -0500

    création de slider

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.2.0/css/all.css">
+    <script src="slider.js"defer></script>
 
     <?php wp_head(); ?>
 </head>

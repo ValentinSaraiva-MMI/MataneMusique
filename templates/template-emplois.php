31431d2904dd22a4a9842e41a19a06d9a0974c80
valentinsa <valentinsa0701@gmail.com>
2022-12-15 09:56:26 -0500

    template emplois

--- templates/template-emplois.php
+++ templates/template-emplois.php
@@ -2,5 +2,23 @@
 <?php get_header(); ?>
 <h1>Emplois</h1>
 
+<div class="actu-section">
+            <?php if ($query->have_posts()):?>
+            <?php while ($query->have_posts()): $query->the_post(); ?>
+                <article>
+                    <h3>
+                        <?php the_field('titre_de_lemploi_'); ?>
+                    </h3>
+                    <div class="actu-contient">            
+                        <div>
+                            <?php the_field('presentation_de_lemploi_'); ?>
+                        </div>
+                    </div>
+                    <hr class="header_hr">
+                </article>
+            <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+
 
 <?php get_footer(); ?>

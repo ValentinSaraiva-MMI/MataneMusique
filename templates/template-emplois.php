38ee8e6960af02b0d89733d9b43f553066b5d121
valentinsa <valentinsa0701@gmail.com>
2022-12-16 09:37:19 -0500

    TEST EMPLOIS

--- templates/template-emplois.php
+++ templates/template-emplois.php
@@ -19,21 +19,29 @@
 
 
 
-            <?php if ($query->have_posts()):?>
-            <?php while ($query->have_posts()): $query->the_post(); ?>
-                <article>
-                    <h3>
-                        <?php the_field('titre_de_lemploi_'); ?>
-                    </h3>
-                    <div>            
-                        <div>
-                            <?php the_field('presentation_de_lemploi_'); ?>
-                        </div>
-                    </div>
-                    <hr class="header_hr">
-                </article>
-            <?php endwhile; ?>
-            <?php endif; ?>
+<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+    
+    <article class="post">
+      <?php the_post_thumbnail(); ?>
+
+      <h1><?php the_title(); ?></h1>
+
+      <div class="post__meta">
+        <?php echo get_avatar( get_the_author_meta( 'ID' ), 40 ); ?>
+        <p>
+          Publié le <?php the_date(); ?>
+          par <?php the_author(); ?>
+          Dans la catégorie <?php the_category(); ?>
+          Avec les étiquettes <?php the_tags(); ?>
+        </p>
+      </div>
+
+      <div class="post__content">
+        <?php the_content(); ?>
+      </div>
+    </article>
+
+  <?php endwhile; endif; ?>
         </div>
 
 

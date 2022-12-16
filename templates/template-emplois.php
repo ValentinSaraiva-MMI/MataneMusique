2924976a7602fc98ad96723c48e14261a3aa8dd7
valentinsa <valentinsa0701@gmail.com>
2022-12-16 09:48:54 -0500

    template emplois

--- templates/template-emplois.php
+++ templates/template-emplois.php
@@ -19,7 +19,7 @@
 
 
 
-<?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+             <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
     
     <article class="post">
       <?php the_post_thumbnail(); ?>

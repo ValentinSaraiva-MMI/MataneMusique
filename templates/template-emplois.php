2aa2c9b5275051a1e763c77fbaf7317121fa9642
valentinsa <valentinsa0701@gmail.com>
2022-12-16 10:16:02 -0500

    test post 2

--- templates/template-emplois.php
+++ templates/template-emplois.php
@@ -26,7 +26,14 @@
       <?php the_post_thumbnail(); ?>
 
       
-<!-- --> 
+
+      <h2><?php the_field('titre_de_lemploi_'); ?></h2>
+
+
+
+      <div class="post__content">
+        <?php the_field('presentation_de_lemploi_'); ?>
+      </div>
     </article>
 
   <?php endwhile; endif; ?>
